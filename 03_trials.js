eddd46f7d702c328d87f433a85561442640f689e
pschreich <49302701+pschreich@users.noreply.github.com>
2019-05-15 15:28:39 +0200

    Updated trial.js version

---
+++ 03_trials.js
@@ -0,0 +1,67 @@
+// In this file you can specify the trial data for your experiment
+
+
+const trial_info = {
+    key_press: [
+        {
+            question: "Are these two figures the same expect for their orientation?",
+            picture: "images/1_50_different.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'different',
+            number: '1_50',
+            rotation: '90',
+            actual: 'different'
+        },
+        {
+            question: "Are these two figures the same expect for their orientation?",
+            picture: "images/4_50_same.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'same',
+            number: '4_50',
+            rotation: '90',
+            actual: 'same'
+        },
+        {
+            question: "Are these two figures the same expect for their orientation?",
+            picture: "images/6_50_same.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'same',
+            number: '6_50',
+            rotation: '90',
+            actual: 'same'
+        },
+        {
+            question: "Are these two figures the same expect for their orientation?",
+            picture: "images/9_150_different.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'different',
+            number: '9_150',
+            rotation: '180',
+            actual: 'different'
+        },
+        {
+            question: "Are these two figures the same expect for their orientation?",
+            picture: "images/6_150_different.jpg",
+            key1: 'f',
+            key2: 'j',
+            f: 'same',
+            j: 'different',
+            expected: 'different',
+            number: '6_150',
+            rotation: '180',
+            actual: 'different'
+        }
+    ]
+};
